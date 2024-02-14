528a864dd283089ce3cf273c9a88a130c50d874a
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2024-02-14 13:24:00 +0100

    More sgfs

--- SGFs-to-JS-List/Program.cs
+++ SGFs-to-JS-List/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace SGFs_to_JS_List
+﻿using System.Text;
+
+namespace SGFs_to_JS_List
 {
     internal class Program
     {
@@ -8,18 +10,23 @@
             IEnumerable<string> sgfPaths = Directory.EnumerateFiles(sgfRootPath, "*.sgf", SearchOption.AllDirectories);
 
             int i = 0;
+            StringBuilder stringBuilder = new();
             foreach (string sgfPath in sgfPaths)
             {
                 string sgfContent = File.ReadAllText(sgfPath);
                 // remove (;FF[4]GM[1]SZ[19]PB[]PW[]HA[0]KM[6.5]RU[Japanese];
                 sgfContent = sgfContent.Substring(51);
 
-                Console.WriteLine("\t\"" + sgfContent + "\",");
+                stringBuilder.Append("\t\"");
+                stringBuilder.Append(sgfContent);
+                stringBuilder.AppendLine("\",");
 
                 i++;
                 //if (i == 50)
                 //    break;
             }
+
+            File.WriteAllText("sgfs.txt", stringBuilder.ToString());
         }
     }
 }
